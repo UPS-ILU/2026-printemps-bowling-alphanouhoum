759ddb0d0ac4e3533687596a2c77d51983fa2c60
alphanouhoum <alpha-nouhoum.toure@utoulouse.fr>
2026-04-16 17:40:40 +0200

    ITER_1

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,5 +1,16 @@
 package tdd;
 
 public class Game {
-
+	
+	private int score = 0;
+	
+	public void roll(int nbQuilleTomber) {
+		//TODO
+	}
+	
+	public int scoreGame() {
+		return score;
+	}
+	
+	
 }
--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -2,13 +2,21 @@ package tdd;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
 
+	private Game game;
+	
+	@BeforeEach
+	void initialiserGame() {
+		game = new Game();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void entrerVide() {
+		assertEquals(0, game.scoreGame());
 	}
 
 }
