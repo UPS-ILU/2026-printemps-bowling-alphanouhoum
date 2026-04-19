012acd62aaaa4e850e9a223947006254c2f3f76b
alphanouhoum <alpha-nouhoum.toure@utoulouse.fr>
2026-04-19 20:01:54 +0200

    ITER_8

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -4,72 +4,38 @@ import java.util.Iterator;
 
 public class Game {
 	
-	private int score = 0;
-	private int scoreBonus = 0;
-	private int lancer = 1;
-	private int[] tabScore = new int[20];
-	private boolean spare = false;
-	private boolean strike = false;
-	private int strikeTour;
+	 private int[] tabScore = new int[30]; // max 30 lancers
+	 private int lancer = 0;
+
+	 public void roll(int nbQuilleTomber) {
+		 tabScore[lancer] = nbQuilleTomber;
+		 lancer++;
+	 }
 	
-	
-	
-	public void roll(int nbQuilleTomber) {
-		tabScore[lancer-1] = nbQuilleTomber;
-		
-		if (strike && strikeTour != 0) {
-			scoreBonus += nbQuilleTomber;
-			strikeTour--;
-			
-		}else {
-			score += scoreBonus;
-			scoreBonus = 0;
-			strike = false;
-		}
-		
-		if (spare) {
-			score += nbQuilleTomber*2;
-			spare = false;
-		}else {
-			score += nbQuilleTomber;
-			
-		}
-		
-		
-		
-		
-		/*Spare pour les deux lance lancer*/
-		if (lancer%2 == 0 && nbQuilleTomber + tabScore[lancer-2] == 10) {
-				spare = true;
-			}
-		
-		
-		/*Strike premier lancer*/
-		if (lancer%2 != 0 && nbQuilleTomber == 10) {
-				strike = true;
-				strikeTour = 2; //Score x2 pour lees deux prochain tour
-				lancer++;
-			}
-		
-		
-		//System.out.println("Quille Tomber = "+ nbQuilleTomber + " Strike ? "+ strike);
-		//System.out.println("Srore au lancer "+ lancer + " = "+ score);
-		//System.out.println("Nb lancer ="+lancer+" "+spare);
-		
-		
-		
-		lancer++;
-	}
 	
 	
 	
 	
 	
 	public int scoreGame() {
-		for (int i = 0; i < tabScore.length; i++) {
-			System.out.print(tabScore[i] + ", ");
+		int score = 0;
+		int indexLancer = 0;
+		for (int tour = 0; tour < 10; tour++) {
+			//Strike
+			if (tabScore[indexLancer] == 10) {
+				score += 10 + tabScore[indexLancer+1] + tabScore[indexLancer+2];
+				indexLancer+= 1;
+			//spare
+			} else if (tabScore[indexLancer] + tabScore[indexLancer+1] == 10) {
+				score += 10 + tabScore[indexLancer+2];
+				indexLancer += 2;
+			} else {
+				score += tabScore[indexLancer] + tabScore[indexLancer+1];
+				indexLancer+=2;
+				
+			}
 		}
-		System.out.println();
+		
 		return score;
 	}
 	
